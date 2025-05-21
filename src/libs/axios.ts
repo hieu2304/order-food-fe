cca937be70662efa6d900214855bc9ad51916fc4
hieu.dang <>
2025-05-21 11:30:13 +0700

    feat: update proxy vite config

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.DEV ? "/api" : import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
